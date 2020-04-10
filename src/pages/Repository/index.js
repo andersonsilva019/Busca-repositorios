c1b1f6cd21470945fb9067238f6ac3d94016597b
Anderson0017 <andeerson0017@gmail.com>
2020-04-10 11:47:11 -0300

    Exibindo issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import Proptypes from 'prop-types'
 
-import { Loading, Owner } from './styles'
+import { Loading, Owner, IssuesList } from './styles'
 import  Container  from '../../components/Container/'
 
 export default class Repository extends Component {
@@ -66,6 +66,24 @@ export default class Repository extends Component {
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+
+        <IssuesList>
+          {issues.map( issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login}/>
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssuesList>
+
       </Container>
     )
   }
